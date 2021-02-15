abb08794ae246b9b1a4a4c09b74b48e93bc46681
Nicole Pompei <nicolepompei@Nicoles-MacBook-Pro-2.local>
2021-02-15 15:37:01 -0500

    added table data and table headers, wrote mark up in table html file to display tables on elements collection

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data: any = [
+    { name: 'Jon', age: 25, job: 'Engineer', employed: true },
+    { name: 'Jessie', age: 22, job: 'Designer', employed: false },
+    { name: 'Kyle', age: 28, job: 'Engineer', employed: true },
+  ];
+  headers: any = [
+    { key: 'employed', label: 'Has a job?' },
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CollectionsHomeComponent, TableComponent],
-  imports: [
-    CommonModule,
-    CollectionsRoutingModule
-  ],
-  exports: [
-  ]
+  imports: [SharedModule, CommonModule, CollectionsRoutingModule],
+  exports: [],
 })
-export class CollectionsModule { }
+export class CollectionsModule {}
