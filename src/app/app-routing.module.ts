d998057a5a5fe8cdf31da6ea9c4d705f5810da28
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-02-15 20:05:53 -0500

    generated views home and statistics components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,30 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-
 const routes: Routes = [
-  {path: 'elements', 
-    loadChildren: () => 
-      import('./elements/elements.module').then((m => m.ElementsModule))
+  {
+    path: 'elements',
+    loadChildren: () =>
+      import('./elements/elements.module').then((m) => m.ElementsModule),
   },
   {
     path: 'collections',
-      loadChildren: () =>
-        import('./collections/collections.module').then((m => m.CollectionsModule))
+    loadChildren: () =>
+      import('./collections/collections.module').then(
+        (m) => m.CollectionsModule
+      ),
   },
-  { path: '', component: HomeComponent},
-  {path: '**', component: NotFoundComponent}
+  {
+    path: 'views',
+    loadChildren: () =>
+      import('./views/views.module').then((m) => m.ViewsModule),
+  },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ViewsHomeComponent } from './views-home/views-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [StatisticsComponent, ViewsHomeComponent],
   imports: [
     CommonModule,
     ViewsRoutingModule
